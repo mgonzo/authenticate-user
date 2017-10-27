7a1893378f5b65a706a42c0cd6ee1ef97101cb0b
Marcos Gonzales <marcos@Marcoss-MacBook-Air.local>
2017-10-26 20:16:27 -0700

    added test placeholder

---
+++ tests/services-authenticate-tests.js
@@ -0,0 +1,14 @@
+var test = require('tape');
+
+test('generate token test', function (t) {
+    t.plan(1);
+    t.equal(false, true);
+});
+
+test('validate token test', function (t) {
+    // validate correct format
+    // validate not expired
+    // invalidate expired
+    t.plan(1);
+    t.equal(false, true);
+});
